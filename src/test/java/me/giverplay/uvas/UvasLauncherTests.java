510b8a2f871c22925bccf84824cb4749c4c2a621
GiverPlay007 <play.giver.007@gmail.com>
2023-11-16 21:58:36 -0300

    Generate project with Spring Initializr

---
+++ src/test/java/me/giverplay/uvas/UvasLauncherTests.java
@@ -0,0 +1,13 @@
+package me.giverplay.uvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UvasLauncherTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
