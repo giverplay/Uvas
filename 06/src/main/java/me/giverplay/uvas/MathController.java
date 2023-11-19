189c44b43b83a4939f092896e737962a8699f445
GiverPlay007 <play.giver.007@gmail.com>
2023-11-19 10:24:23 -0300

    Validate path variables and return sum

--- 06/src/main/java/me/giverplay/uvas/MathController.java
+++ 06/src/main/java/me/giverplay/uvas/MathController.java
@@ -12,7 +12,28 @@ public class MathController {
   private final AtomicLong ids = new AtomicLong();
 
   @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-  public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
-    return 10.0D;
+  public Double sum(@PathVariable(value = "numberOne") String numberOne,
+                    @PathVariable(value = "numberTwo") String numberTwo)
+    throws Exception {
+
+    if (isNaN(numberOne) || isNaN(numberTwo)) {
+      throw new Exception();
+    }
+
+    return parseDouble(numberOne) + parseDouble(numberTwo);
+  }
+
+  private double parseDouble(String value) {
+    if(value == null) return 0.0D;
+
+    value = value.replaceAll(",", ".");
+    return Double.parseDouble(value);
+  }
+
+  private boolean isNaN(String value) {
+    if(value == null) return true;
+
+    value = value.replaceAll(",", ".");
+    return !value.matches("[-+]?[0-9]*\\.?[0-9]+");
   }
 }
