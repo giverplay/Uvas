a0b38d842800fa0f825b8d28bc180ebcf4f598e6
GiverPlay007 <play.giver.007@gmail.com>
2023-11-19 11:02:00 -0300

    Implement subtraction route

--- 06/src/main/java/me/giverplay/uvas/MathController.java
+++ 06/src/main/java/me/giverplay/uvas/MathController.java
@@ -15,7 +15,7 @@ public class MathController {
   @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
   public Double sum(@PathVariable(value = "numberOne") String numberOne,
                     @PathVariable(value = "numberTwo") String numberTwo)
-    throws Exception {
+    throws UnsupportedMathOperationException {
 
     if (isNaN(numberOne) || isNaN(numberTwo)) {
       throw new UnsupportedMathOperationException("The sum terms must be numbers");
@@ -24,15 +24,27 @@ public class MathController {
     return parseDouble(numberOne) + parseDouble(numberTwo);
   }
 
+  @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+  public Double subtraction(@PathVariable(value = "numberOne") String numberOne,
+                            @PathVariable(value = "numberTwo") String numberTwo)
+    throws UnsupportedMathOperationException {
+
+    if (isNaN(numberOne) || isNaN(numberTwo)) {
+      throw new UnsupportedMathOperationException("The subtraction terms must be numbers");
+    }
+
+    return parseDouble(numberOne) - parseDouble(numberTwo);
+  }
+
   private double parseDouble(String value) {
-    if(value == null) return 0.0D;
+    if (value == null) return 0.0D;
 
     value = value.replaceAll(",", ".");
     return Double.parseDouble(value);
   }
 
   private boolean isNaN(String value) {
-    if(value == null) return true;
+    if (value == null) return true;
 
     value = value.replaceAll(",", ".");
     return !value.matches("[-+]?[0-9]*\\.?[0-9]+");
